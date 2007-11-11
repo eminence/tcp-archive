afceb187dab715c7548982e4f3791188f89f6027
bdiamond <bdiamond@3089d59f-7381-4705-825a-edebe9fe821e>
2007-11-11 07:37:58 +0000

    This is the (eventual) interface that we'll use to implement sequencen umber shit and then sliding
     winder
    
               mouther
    
     
     falkers

---
+++ cbuffer.h
@@ -0,0 +1,32 @@
+#ifndef CBUFFER_H
+#define CBUFFER_H
+
+#include <assert.h>
+
+typedef struct {
+  char* data;
+  int size;
+} cbuf_t;
+
+/* Creation. */
+cbuf_t* cbuf_new(int size);
+void cbuf_destroy();
+
+/* Operations. */
+char  cbuf_put(cbuf_t* buf, int index, char value);
+char  cbuf_get(cbuf_t* buf, int index);
+char  cbuf_put_range(cbuf_t* buf, int start, int len);
+char* cbuf_get_range(cbuf_t* buf, int start, int len);
+
+#define cbuf_size(c)            ((c)->size)
+#define cbuf_mod(c,n)           ((n) % cbuf_size(c))
+#define cbuf_add(c,i,n)         cbuf_mod(c,i+n)
+#define cbuf_sub(c,i,n)         cbuf_mod(c,i-a)
+#define cbuf_inc(c,i)           cbuf_add(c,i,1)
+#define cbuf_dec(c,i)           cbuf_sub(c,i,1)
+#define cbuf_cmp(c,a,b)         ((cbuf_mod(c,a) == cbuf_mod(c,b)) ? 0 : \
+                                 (((cbuf_mod(c,a) < cbuf_mod(c,b)) && ((cbuf_mod(c,b))-(cbuf_mod(c,a)) < cbuf_size(c)/2)) \
+                                    || ((cbuf_mod(c,a) > cbuf_mod(c,b)) && ((cbuf_mod(c,a))-(cbuf_mod(c,b)) > cbuf_size(c)/2))) ? -1 : 1)
+#define cbuf_contains(c,a,b,n)  (cbuf_cmp(c,a,b) 
+
+#endif
