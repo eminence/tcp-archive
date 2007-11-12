11d8f02415bcf6356a564534fe368568c4203c7d
bdiamond <bdiamond@3089d59f-7381-4705-825a-edebe9fe821e>
2007-11-12 14:22:13 +0000

    CIRCULAR BUFFERS ARE NOW
                E     X      T      R       E       M      E

--- cbuffer.c
+++ cbuffer.c
@@ -3,13 +3,14 @@
 #include <assert.h>
 
 #include "cbuffer.h"
+#include "tcppacket.h"
 
 cbuf_t* cbuf_new(int size) {
   cbuf_t* buf = malloc(sizeof(cbuf_t));
   assert(buf);
   assert(0 < size);
 
-  buf->data = malloc(size);
+  buf->data = malloc(size*2);
   assert(buf->data);
 
   buf->size = size;
@@ -22,60 +23,77 @@ void cbuf_destroy(cbuf_t* buf) {
   free(buf);
 }
 
-char cbuf_put(cbuf_t* buf, int index, char value) {
-  char retval;
-
+void __cbuf_put(cbuf_t* buf, int index, char value, uint8_t type) {
   assert(0 <= index);
-
-  retval = buf->data[cbuf_mod(buf, index)];
-  buf->data[cbuf_mod(buf, index)] = value;
   
-  return retval;
+  buf->data[__cbuf_type(cbuf_mod(buf, index))] = type;
+  buf->data[__cbuf_data(cbuf_mod(buf, index))] = value;
 }
 
-char cbuf_get(cbuf_t* buf, int index) {
+char __cbuf_get(cbuf_t* buf, int index, uint8_t* type) {
   assert(0 <= index);
-  return buf->data[cbuf_mod(buf, index)];
+  assert(type);
+  
+  /* Obtain type. */
+  *type = (uint8_t)buf->data[__cbuf_type(cbuf_mod(buf, index))];
+
+  /* Return data. */
+  return buf->data[__cbuf_data(cbuf_mod(buf, index))];
 }
 
 int cbuf_put_range(cbuf_t* buf, char* buffer, int start, int len) {
-  int i, cnt = 0;
+  int cnt = 0, i;
 
   assert(0 <= start);
   assert(0 <= len);
   assert(len <= cbuf_size(buf));
 
-  cbuf_iterate_indices(buf, start, len, i) {
-    cbuf_put(buf, i, buffer[cnt++]);
-  } cbuf_iterate_indices_end();
+  cbuf_r_iterate_indices(buf, start, len, i) {
+    __cbuf_put(buf, i, buffer[cnt++], CBUF_DATA);
+  } cbuf_r_iterate_indices_end();
 
   return 0;
 }
 
-char* cbuf_get_range(cbuf_t* buf, int start, int len) {
-  char* buffer;
+int cbuf_put_flag(cbuf_t* buf, int start, uint8_t flags) {
+   __cbuf_put(buf, start, (char)flags, CBUF_FLAG);
+
+   return 0;
+}
+
+int cbuf_get_range(cbuf_t* buf, int start, int len, void** vdata) {
+  char** data = (char**)vdata;
   char v;
   int cnt = 0;
+  uint8_t t;
 
   assert(0 <= start);
   assert(0 <= len);
   assert(len <= cbuf_size(buf));
 
-  buffer = malloc(len);
-  assert(buffer);
-
-  cbuf_iterate_values(buf, start, len, v) {
-    buffer[cnt++] = v;
+  /* safe bet: never read more than len bytes */
+  *data = malloc(len);
+  assert(data);
+  
+  cbuf_iterate_values(buf, start, len, v, t) {
+    (*data)[cnt++] = v;
   } cbuf_iterate_values_end();
+  
+  *data = realloc(*data, cnt);
+  
+  assert(*data);
 
-  return buffer;
+  return t == CBUF_DATA ? cnt : -1;
 }
 
 void cbuf_print(cbuf_t* buf) {
-  char c;
+  uint8_t c;
+  int i=1;
+  int t;
+
   printf("[...] ");
-  cbuf_iterate_values(buf, 0, cbuf_size(buf), c) {
-    printf("%c ", c);
-  } cbuf_iterate_values_end();
+  cbuf_r_iterate_values(buf, 0, cbuf_size(buf), c) {
+    printf("%d ", c);
+  } cbuf_r_iterate_values_end();
   printf("[...]\n");
 }
--- cbuffer.h
+++ cbuffer.h
@@ -2,6 +2,10 @@
 #define CBUFFER_H
 
 #include <assert.h>
+#include <stdint.h>
+
+#define CBUF_DATA     0x01
+#define CBUF_FLAG     0x02
 
 typedef struct {
   char* data;
@@ -13,37 +17,54 @@ cbuf_t* cbuf_new(int size);
 void cbuf_destroy(cbuf_t* buf);
 
 /* Operations. */
-char  cbuf_put(cbuf_t* buf, int index, char value);
-char  cbuf_get(cbuf_t* buf, int index);
 int   cbuf_put_range(cbuf_t* buf, char* buffer, int start, int len);
-char* cbuf_get_range(cbuf_t* buf, int start, int len);
+int   cbuf_put_flag(cbuf_t* buf, int start, uint8_t flags);
+int   cbuf_get_range(cbuf_t* buf, int start, int len, void** data);
 void  cbuf_print(cbuf_t* buf);
 
-#define cbuf_size(c)                          ((c)->size)
-#define cbuf_mod(c,n)                         ((n) % cbuf_size(c))
-#define cbuf_add(c,i,n)                       cbuf_mod(c,i+n)
-#define cbuf_sub(c,i,n)                       cbuf_mod(c,i-a)
-#define cbuf_inc(c,i)                         ((i)=cbuf_add(c,i,1))
-#define cbuf_dec(c,i)                         ((i)=cbuf_sub(c,i,1))
-#define cbuf_cmp(c,a,b)                       ((cbuf_mod(c,a) == cbuf_mod(c,b)) ? 0 : \
-                                                (((cbuf_mod(c,a) < cbuf_mod(c,b)) && ((cbuf_mod(c,b))-(cbuf_mod(c,a)) < cbuf_size(c)/2)) \
-                                                   || ((cbuf_mod(c,a) > cbuf_mod(c,b)) && ((cbuf_mod(c,a))-(cbuf_mod(c,b)) > cbuf_size(c)/2))) ? -1 : 1)
-#define cbuf_lt(c,a,b)                        (cbuf_cmp(c,a,b) == -1)                                    
-#define cbuf_gt(c,a,b)                        (cbuf_cmp(c,a,b) ==  1) /* All comparisons cbuf_@(c,a,b) are a @ b. */                                    
-#define cbuf_eq(c,a,b)                        (cbuf_cmp(c,a,b) ==  0)                                    
-#define cbuf_lte(c,a,b)                       (cbuf_lt(c,a,b) || cbuf_eq(c,a,b))
-#define cbuf_gte(c,a,b)                       (cbuf_gt(c,a,b) || cbuf_eq(c,a,b))
-#define cbuf_incl_contains(c,a,b,n)           (cbuf_lte(c,a,n) && cbuf_gte(c,b,n))
-#define cbuf_excl_contains(c,a,b,n)           (cbuf_lt(c,a,n) && cbuf_gt(c,b,n))
-#define cbuf_btm_contains(c,a,b,n)            (cbuf_lte(c,a,n) && cbuf_gt(c,b,n))
-#define cbuf_top_contains(c,a,b,n)            (cbuf_lt(c,a,n) && cbuf_gte(c,b,n))
+/* Private function. */
+char  __cbuf_get(cbuf_t* buf, int index, uint8_t* type);
+void  __cbuf_put(cbuf_t* buf, int index, char value, uint8_t type);
 
-#define cbuf_loop(c,start,len,i,v)            do { int __cbuf_c = 0; \
-                                                for(i=cbuf_mod(c,start),v=cbuf_get(c,i); __cbuf_c < len; cbuf_inc(c,i),v=cbuf_get(c,i), __cbuf_c++)
-#define cbuf_loop_end()                       } while(0)
-#define cbuf_iterate_indices(c,start,len,var) do { char __cbuf_v; cbuf_loop(c,start,len,var,__cbuf_v)
-#define cbuf_iterate_indices_end()            cbuf_loop_end(); } while(0)
-#define cbuf_iterate_values(c,start,len,var)  do { int __cbuf_i; cbuf_loop(c,start,len,__cbuf_i,var)
-#define cbuf_iterate_values_end()             cbuf_loop_end(); } while(0)
+#define __cbuf_type(i)                          (2*(i))
+#define __cbuf_data(i)                          (2*(i)+1)
+#define cbuf_size(c)                            ((c)->size)
+#define cbuf_mod(c,n)                           ((n) % cbuf_size(c))
+#define cbuf_add(c,i,n)                         cbuf_mod(c,i+n)
+#define cbuf_sub(c,i,n)                         cbuf_mod(c,i-a)
+#define cbuf_inc(c,i)                           ((i)=cbuf_add(c,i,1))
+#define cbuf_dec(c,i)                           ((i)=cbuf_sub(c,i,1))
+#define cbuf_cmp(c,a,b)                         ((cbuf_mod(c,a) == cbuf_mod(c,b)) ? 0 : \
+                                                  (((cbuf_mod(c,a) < cbuf_mod(c,b)) && ((cbuf_mod(c,b))-(cbuf_mod(c,a)) < cbuf_size(c)/2)) \
+                                                     || ((cbuf_mod(c,a) > cbuf_mod(c,b)) && ((cbuf_mod(c,a))-(cbuf_mod(c,b)) > cbuf_size(c)/2))) ? -1 : 1)
+#define cbuf_lt(c,a,b)                          (cbuf_cmp(c,a,b) == -1)                                    
+#define cbuf_gt(c,a,b)                          (cbuf_cmp(c,a,b) ==  1) /* All comparisons cbuf_@(c,a,b) are a @ b. */                                    
+#define cbuf_eq(c,a,b)                          (cbuf_cmp(c,a,b) ==  0)                                    
+#define cbuf_lte(c,a,b)                         (cbuf_lt(c,a,b) || cbuf_eq(c,a,b))
+#define cbuf_gte(c,a,b)                         (cbuf_gt(c,a,b) || cbuf_eq(c,a,b))
+#define cbuf_incl_contains(c,a,b,n)             (cbuf_lte(c,a,n) && cbuf_gte(c,b,n))
+#define cbuf_excl_contains(c,a,b,n)             (cbuf_lt(c,a,n) && cbuf_gt(c,b,n))
+#define cbuf_btm_contains(c,a,b,n)              (cbuf_lte(c,a,n) && cbuf_gt(c,b,n))
+#define cbuf_top_contains(c,a,b,n)              (cbuf_lt(c,a,n) && cbuf_gte(c,b,n))
+
+#define cbuf_loop(c,start,len,i,v,t)            do { int __cbuf_c=0,  __cbuf_l=1; uint8_t __cbuf_t; \
+                                                for(i=cbuf_mod(c,start),v=__cbuf_get(c,i,&t),__cbuf_t=t;\
+                                                    __cbuf_c < len && (__cbuf_t == t) && __cbuf_l; \
+                                                    cbuf_inc(c,i),v=__cbuf_get(c,i,&__cbuf_t),__cbuf_l=__cbuf_t==CBUF_DATA,__cbuf_c++)
+#define cbuf_loop_end()                         } while(0)
+#define cbuf_iterate_indices(c,start,len,var,t) do { char __cbuf_v; cbuf_loop(c,start,len,var,__cbuf_v,t)
+#define cbuf_iterate_indices_end()              cbuf_loop_end(); } while(0)
+#define cbuf_iterate_values(c,start,len,var,t)  do { int __cbuf_i; cbuf_loop(c,start,len,__cbuf_i,var,t)
+#define cbuf_iterate_values_end()               cbuf_loop_end(); } while(0)
+
+#define cbuf_r_loop(c,start,len,i,v)            do { int __cbuf_c = 0; uint8_t  __cbuf_t; \
+                                                for(i=cbuf_mod(c,start),v=__cbuf_get(c,i,&__cbuf_t);\
+                                                    __cbuf_c < len; \
+                                                    cbuf_inc(c,i),v=__cbuf_get(c,i,&__cbuf_t),__cbuf_c++)
+#define cbuf_r_loop_end()                         } while(0)
+#define cbuf_r_iterate_indices(c,start,len,var) do { char __cbuf_v; cbuf_r_loop(c,start,len,var,__cbuf_v)
+#define cbuf_r_iterate_indices_end()              cbuf_r_loop_end(); } while(0)
+#define cbuf_r_iterate_values(c,start,len,var)  do { int __cbuf_i; cbuf_r_loop(c,start,len,__cbuf_i,var)
+#define cbuf_r_iterate_values_end()               cbuf_r_loop_end(); } while(0)
 
 #endif
