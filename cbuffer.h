ca3eb84489a5f94915685b110f409f7cf2eb2159
bdiamond <bdiamond@3089d59f-7381-4705-825a-edebe9fe821e>
2007-11-13 01:50:57 +0000

    fixed idiotic partial change issue

--- cbuffer.h
+++ cbuffer.h
@@ -23,8 +23,8 @@ int   cbuf_get_range(cbuf_t* buf, int start, int len, void** data);
 void  cbuf_print(cbuf_t* buf);
 
 /* Private function. */
-char  __cbuf_get(cbuf_t* buf, int index, uint8_t* type, int unsafe);
-void  __cbuf_put(cbuf_t* buf, int index, char value, uint8_t type, int unsafe);
+char  __cbuf_get(cbuf_t* buf, int index, uint8_t* type);
+void  __cbuf_put(cbuf_t* buf, int index, char value, uint8_t type);
 
 #define __cbuf_type(i)                          (2*(i))
 #define __cbuf_data(i)                          (2*(i)+1)
@@ -48,9 +48,9 @@ void  __cbuf_put(cbuf_t* buf, int index, char value, uint8_t type, int unsafe);
 #define cbuf_top_contains(c,a,b,n)              (cbuf_lt(c,a,n) && cbuf_gte(c,b,n))
 
 #define cbuf_loop(c,start,len,i,v,t)            do { int __cbuf_c=0,  __cbuf_l=1; uint8_t __cbuf_t; \
-                                                for(i=cbuf_mod(c,start),v=__cbuf_get(c,i,&t,1),__cbuf_t=t;\
+                                                for(i=cbuf_mod(c,start),v=__cbuf_get(c,i,&t),__cbuf_t=t;\
                                                     __cbuf_c < len && (__cbuf_t == t) && __cbuf_l; \
-                                                    cbuf_inc(c,i),v=__cbuf_get(c,i,&__cbuf_t,1),__cbuf_l=__cbuf_t==CBUF_DATA,__cbuf_c++)
+                                                    cbuf_inc(c,i),v=__cbuf_get(c,i,&__cbuf_t),__cbuf_l=__cbuf_t==CBUF_DATA,__cbuf_c++)
 #define cbuf_loop_end()                         } while(0)
 #define cbuf_iterate_indices(c,start,len,var,t) do { char __cbuf_v; cbuf_loop(c,start,len,var,__cbuf_v,t)
 #define cbuf_iterate_indices_end()              cbuf_loop_end(); } while(0)
@@ -58,9 +58,9 @@ void  __cbuf_put(cbuf_t* buf, int index, char value, uint8_t type, int unsafe);
 #define cbuf_iterate_values_end()               cbuf_loop_end(); } while(0)
 
 #define cbuf_r_loop(c,start,len,i,v)            do { int __cbuf_c = 0; uint8_t  __cbuf_t; \
-                                                for(i=cbuf_mod(c,start),v=__cbuf_get(c,i,&__cbuf_t,1);\
+                                                for(i=cbuf_mod(c,start),v=__cbuf_get(c,i,&__cbuf_t);\
                                                     __cbuf_c < len; \
-                                                    cbuf_inc(c,i),v=__cbuf_get(c,i,&__cbuf_t,1),__cbuf_c++)
+                                                    cbuf_inc(c,i),v=__cbuf_get(c,i,&__cbuf_t),__cbuf_c++)
 #define cbuf_r_loop_end()                         } while(0)
 #define cbuf_r_iterate_indices(c,start,len,var) do { char __cbuf_v; cbuf_r_loop(c,start,len,var,__cbuf_v)
 #define cbuf_r_iterate_indices_end()              cbuf_r_loop_end(); } while(0)
